87ba86b1df5b190089bd00aa756d4b7f4e75c2ef
axelcisnero <axel.cisnero@hotmail.com>
2025-03-18 15:59:13 -0500

    Creación de funcion agregarAmigo que agrega los nombres al arreglo.

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -1,6 +1,14 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-let arregloAmigos [];
+let listaAmigos = [];
 
-function agregarAmigos (){
+function agregarAmigo (){
+    let ingresoAmigo = document.getElementById("amigo");
 
+    if (ingresoAmigo.value === ""){
+        alert ("¡Debes ingresar un nombre válido!")
+        return;
+    }
+        listaAmigos.push(ingresoAmigo.value);
+        ingresoAmigo.value = "";
+        console.log(listaAmigos);
 }
